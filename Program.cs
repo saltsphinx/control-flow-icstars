92b0b23fb590845678678e1d254f462a922d7532
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-08-10 21:52:29 -0500

    Fix if/else solution

--- Program.cs
+++ Program.cs
@@ -54,10 +54,10 @@ Console.WriteLine("Your grade is: " + grade);
 // else if (score >= 80) {
 //     grade = 'B';
 // }
-// else if (score >= 80) {
+// else if (score >= 70) {
 //     grade = 'C';
 // }
-// else if (score >= 80) {
+// else if (score >= 60) {
 //     grade = 'D';
 // }
 // else {
